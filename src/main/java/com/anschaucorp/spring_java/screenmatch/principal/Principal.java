06f3a547338e4c47fb235bcc21b766bb05a5ad0f
Querino Anschau Junior <querinojunior@gmail.com>
2024-08-07 15:41:36 -0300

    Final class

--- src/main/java/com/anschaucorp/spring_java/screenmatch/principal/Principal.java
+++ src/main/java/com/anschaucorp/spring_java/screenmatch/principal/Principal.java
@@ -6,11 +6,13 @@ import com.anschaucorp.spring_java.screenmatch.model.DadosTemporada;
 import com.anschaucorp.spring_java.screenmatch.model.Episode;
 import com.anschaucorp.spring_java.screenmatch.service.ConsumoAPI;
 import com.anschaucorp.spring_java.screenmatch.service.ConverteDados;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import javax.sound.midi.Soundbank;
+import javax.swing.text.DateFormatter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -54,12 +56,17 @@ public class Principal {
                 .flatMap(t -> t.episodioList().stream())
                 .collect(Collectors.toList());
 
-        System.out.println("\nTop 5 episodes");
-        dadosEpisodios.stream()
-                .filter(e -> !e.rate().equalsIgnoreCase("N/A"))
-                .sorted(Comparator.comparing(DadosEpisodio::rate).reversed())
-                .limit(5)
-                .forEach(System.out::println);
+//        System.out.println("\nTop 10 episodes");
+//        dadosEpisodios.stream()
+//                .filter(e -> !e.rate().equalsIgnoreCase("N/A"))
+//                //.peek(e -> System.out.println("Filter 1(N/A): " + e))
+//                .sorted(Comparator.comparing(DadosEpisodio::rate).reversed())
+//                //.peek(e -> System.out.println("Sort: " + e))
+//                .limit(10)
+//                //.peek(e -> System.out.println("Limit: " + e))
+//                .map(e -> e.title().toUpperCase())
+//                //.peek(e -> System.out.println("Map:" + e))
+//                .forEach(System.out::println);
 
         List<Episode> episodes = temporadas.stream()
                 .flatMap(t -> t.episodioList().stream()
@@ -68,5 +75,52 @@ public class Principal {
 
         episodes.forEach(System.out::println);
 
+//        System.out.println("Digite uma parte to title do episodio: ");
+//
+//        var partOfTitle = leitura.nextLine();
+//
+//        Optional<Episode> searchedEpisode = episodes.stream()
+//                .filter(e -> e.getTitle().toUpperCase().contains(partOfTitle.toUpperCase()))
+//                .findFirst();
+//        if(searchedEpisode.isPresent()){
+//            System.out.println("Episodio encontrado");
+//            System.out.println("Temporada " + searchedEpisode.get().getSeason());
+//        } else {
+//            System.out.println("Episodio não encontrado");
+//        }
+//
+//        System.out.println("A partir de que ano você quer ver os episódios: ");
+//        var ano = leitura.nextInt();
+//        leitura.nextLine();
+//
+//        LocalDate dataBusca = LocalDate.of(ano,1,1);
+//
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//
+//        episodes.stream()
+//                .filter(e -> e.getReleaseDate() != null && e.getReleaseDate().isAfter(dataBusca))
+//                .forEach(e -> System.out.println(
+//                        "Sesson: " + e.getSeason() +
+//                                " - Episode: " + e.getEpisode() +
+//                                " - Release date: " + e.getReleaseDate().format(formatter)
+//                ));
+
+        Map<Integer, Double> rateBySeason = episodes.stream()
+                .filter(e -> e.getRate() > 0.0)
+                .collect(Collectors.groupingBy(Episode::getSeason,
+                        Collectors.averagingDouble(Episode::getRate)));
+        System.out.println(rateBySeason);
+
+        DoubleSummaryStatistics est = episodes.stream()
+                .filter(e -> e.getRate() > 0.0)
+                .collect(Collectors.summarizingDouble(Episode::getRate));
+        System.out.println(est);
+
+        System.out.println("Average: " + est.getAverage());
+        System.out.println("Melhor avaliação: " + est.getMax() );
+        episodes.stream().filter(e -> e.getRate() == est.getMax()).forEach(e -> System.out.println(e.getTitle()));
+        System.out.println("Pior avaliação: " + est.getMin());
+        episodes.stream().filter(e -> e.getRate() == est.getMin()).forEach(e -> System.out.println(e.getTitle()));
+        System.out.println("Quantidade: " + est.getCount());
     }
 }
